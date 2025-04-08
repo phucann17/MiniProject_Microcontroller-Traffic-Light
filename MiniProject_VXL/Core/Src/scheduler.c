459934ea88cb8649ad4eb0d580430111e23653ea
phucann17 <139678917+phucann17@users.noreply.github.com>
2025-04-08 22:06:16 +0700

    Update scheduler.c

--- MiniProject_VXL/Core/Src/scheduler.c
+++ MiniProject_VXL/Core/Src/scheduler.c
@@ -1,231 +1,77 @@
 #include "scheduler.h"
+sTasks SCH_tasks_G[SCH_MAX_TASKS];
+uint8_t start = 0;
+uint8_t end = 0;
 
-
-
-// THE ARRAY OF TASK
-static sTask SCH_tasks_G[SCH_MAX_TASKS];
-uint8_t current_index_task;
-//static uint8_t array_Of_Task_ID[SCH_MAX_TASKS];
-//static uint32_t newTaskID = 0;
-//static uint32_t rearQueue = 0;
-//static uint32_t count_SCH_Update = 0;
-
-//static uint32_t Get_New_Task_ID(void);
-//static void TIMER_Init(void);
-
-// ERROR CODE
-uint8_t Error_code_G ;
-uint8_t Error_port ;
-uint8_t Last_error_code_G;
-uint32_t Error_tick_count_G;
-
-void SCH_Init(){
-	uint8_t i;
-	for (i = 0; i <SCH_MAX_TASKS; i++) {
-		SCH_Delete_Task(i);
+void SCH_Init(void){
+	int i;
+	for (i = 0; i < SCH_MAX_TASKS; i++){
+		SCH_tasks_G[i].pTask = 0x0000;
+		SCH_tasks_G[i].Delay = 0;
+		SCH_tasks_G[i].Period = 0;
+		SCH_tasks_G[i].RunMe = 0;
+		SCH_tasks_G[i].isRun = 0;
 	}
-	 // Reset the global error variable
-	 // − SCH_Delete_Task() will generate an error code,
-	 // (because the task array is empty)
-	 Error_code_G= 0;
-	 //Timer_init();
-	 //MX_IWDG_Init(); //Watchdog init
+	start = end = 0;
+}
+
+void SCH_Add_Task(void(*pFunction)(), uint32_t DELAY, uint32_t PERIOD){
+	if ((end + 1) % SCH_MAX_TASKS == start){
+		return;
+	}
+	for (int i = start; i != end; i = (i + 1) % SCH_MAX_TASKS){
+		if (pFunction == SCH_tasks_G[i].pTask){
+			return;
+		}
+	}
+	SCH_tasks_G[end].pTask = pFunction;
+	SCH_tasks_G[end].Delay = DELAY / TICK;
+	SCH_tasks_G[end].Period = PERIOD / TICK;
+	SCH_tasks_G[end].isRun = 1;
+	SCH_tasks_G[end].RunMe = 0;
+	end = (end + 1) % SCH_MAX_TASKS;
+}
+
+void SCH_Delete_Task(uint8_t task_index){
+	if (start == end) return;
+
+	uint8_t i = task_index;
+	while (i != end){
+		uint8_t next = (i + 1) % SCH_MAX_TASKS;
+		SCH_tasks_G[i] = SCH_tasks_G[next];
+		i = next;
+	}
+
+	end = (end - 1 + SCH_MAX_TASKS) % SCH_MAX_TASKS;
 }
 
 void SCH_Update(void){
-	uint32_t Index;
-	  //NOTE: calculations are in *TICKS* (not milliseconds)
-	for (Index = 0; Index <SCH_MAX_TASKS; Index++) {
-	  // Check if there is a task at this location
-		if (SCH_tasks_G[Index].pTask){
-			if (SCH_tasks_G[Index].Delay == 0) {
-				  // The task is due to run
-				  // Inc. the ’RunMe’ flag
-				SCH_tasks_G[Index].RunMe+= 1;
-				if (SCH_tasks_G[Index].Period) {
-					  // Schedule periodic tasks to run again
-					SCH_tasks_G[Index].Delay = SCH_tasks_G[Index].Period;
-				}
-			} else {
-				  // Not yet ready to run: just decrement the delay
-				SCH_tasks_G[Index].Delay --;
+	if (start != end && SCH_tasks_G[start].isRun == 1){
+		if (SCH_tasks_G[start].Delay > 0){
+			SCH_tasks_G[start].Delay--;
+		}
+		else{
+			SCH_tasks_G[start].RunMe = 1;
+
+			if (SCH_tasks_G[start].Period > 0){
+				SCH_tasks_G[start].Delay = SCH_tasks_G[start].Period;
+				SCH_tasks_G[end] = SCH_tasks_G[start];
+				end = (end + 1) % SCH_MAX_TASKS;
 			}
+			start = (start + 1) % SCH_MAX_TASKS;
 		}
 	}
-	//Watchdog_Refresh();
 }
 
-uint32_t SCH_Add_Task(void(* pFunction)(), uint32_t DELAY, uint32_t PERIOD){
-	uint32_t Index = 0;
-	// First find a gap in the array (if there is one)
- while ((SCH_tasks_G[Index].pTask != 0) && (Index < SCH_MAX_TASKS))
- 	 {
-	 	 Index++;
- 	 }
- // Havewe reached the end of the list?
- if (Index ==SCH_MAX_TASKS){
- // Task list is full
- // Set the global error variable
- Error_code_G=ERROR_SCH_TOO_MANY_TASKS;
-  // Also return an error code
- return SCH_MAX_TASKS;
- }
- // If we’re here, there is a space in the task array
- SCH_tasks_G[Index].pTask = pFunction;
- SCH_tasks_G[Index].Delay = DELAY / TICK;
- SCH_tasks_G[Index].Period = PERIOD / TICK;
- SCH_tasks_G[Index].RunMe = 0;
- SCH_tasks_G[Index].TaskID = Index;
- // return position of task (to allow later deletion)
- return Index;
-}
+void SCH_Dispatch_Tasks(void){
+	if (SCH_tasks_G[start].RunMe > 0 && start != end && SCH_tasks_G[start].isRun == 1){
+		SCH_tasks_G[start].RunMe = 0;
 
-uint8_t SCH_Delete_Task(uint32_t TASK_INDEX){
-	uint8_t Return_code;
-	if (SCH_tasks_G[TASK_INDEX].pTask == 0) {
-	 // No task at this location ...
-	 // Set the global error variable
-	 Error_code_G = ERROR_SCH_CANNOT_DELETE_TASK;
-	 // ... also return an error code
-	 Return_code = RETURN_ERROR;
-	 } else {
-	 Return_code = RETURN_NORMAL;
-	 }
-	 SCH_tasks_G[TASK_INDEX].pTask = 0x0000;
-	 SCH_tasks_G[TASK_INDEX]. Delay = 0;
-	 SCH_tasks_G[TASK_INDEX]. Period = 0;
-	 SCH_tasks_G[TASK_INDEX].RunMe = 0;
-	 return Return_code; // return status
-}
+		(*SCH_tasks_G[start].pTask)();
 
-void SCH_Dispatch_Task(void){
-	// Dispatches (runs) the next task (if one is ready)
-	uint8_t Index;
-	 // Dispatches (runs) the next task (if one is ready)
-	for (Index = 0; Index <SCH_MAX_TASKS; Index++){
-		if (SCH_tasks_G[Index].RunMe> 0) {
-			(*SCH_tasks_G[Index].pTask)(); // Run the task
-			SCH_tasks_G[Index].RunMe--; // Reset / reduceRunMe flag
-			// Periodic tasks will automatically run again
-			// − if this is a ’one shot’ task, remove it from the array
-			if (SCH_tasks_G[Index].Period == 0){
-				SCH_Delete_Task(Index);
-			}
+		if (SCH_tasks_G[start].Period == 0) {
+		    start = (start + 1) % SCH_MAX_TASKS;
 		}
+
 	}
-	// Report system status
-	SCH_Report_Status();
-	// The scheduler enters idlemode at this point
-	//SCH_Go_To_Sleep();
 }
-
-void SCH_Go_To_Sleep(){}
-
-void SCH_Report_Status(void) {
-#ifdef SCH_REPORT_ERRORS
-	//ONLYAPPLIES IFWEAREREPORTINGERRORS
-	// Check for answer error code
-	if (Error_code_G != Last_error_code_G) {
-		// Negative logic onLEDs assumed
-		Error_port = 255 - Error_code_G;
-		Last_error_code_G = Error_code_G;
-		if (Error_code_G != 0){
-			Error_tick_count_G= 60000;
-		} else {
-			Error_tick_count_G= 0;
-		}
-	}
-	else {
-		if (Error_tick_count_G != 0){
-			if (--Error_tick_count_G== 0) {
-				Error_code_G= 0; // Reset error code
-			}
-		}
-	}
-#endif
-}
-
-
-/*
-
- voidHAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
- SCH_Update() ;
- }
-
-*/
-
-/*
-
-The dispatcher is the only component in the SuperLoop:
-void main(void){
-while(1){
-	SCH_Dispatch_Tasks() ;
-}
-
-*/
-
-////INIT TIMER_10ms
-//static void TIMER_Init()
-//    cli(); //Disable interupt
-//
-//    /*Reset Timer/counter1*/
-//    TCCR1A = 0;
-//    TCCC1B = 0;
-//    TIMSK = 0;
-//
-//    /*Set up timer/counter 1*/
-//    TCC1B != (1 << CS11)| (1 << CS10); //Prescale = 64
-//    TCNT1 = CONTER_START;
-//    TIMSK1 = (1<<TOIE1); // Overflow interupt enable
-//    sei(); //Enable interupt
-//}
-//
-//ISR (TIMER_OVF_vect){
-//    TCNT1 = CONTER_START;
-//    SCH_Update();
-//}
-//
-//static uint32_t Get_New_Task_ID(void){
-//    newTaskID++;
-//    if(newTaskID == NO_TASK_ID){
-//        newTaskID++;
-//    }
-//    return newTaskID;
-//}
-
-
-//WATCHDOG
-//IWDG_HandleTypeDef hiwdg;
-//static uint32_t counter_for_watchdog = 0;
-//
-//void MX_IWDG_Init(void) {
-//    hiwdg.Instance = IWDG;
-//    hiwdg.Init.Prescaler = IWDG_PRESCALER_32; // Chia tần số để định thời gian cho watchdog
-//    hiwdg.Init.Reload = 4095; // Giá trị reload để định thời gian
-//    if (HAL_IWDG_Init(&hiwdg) != HAL_OK) {
-//        Error_Handler(); // Gọi hàm xử lý lỗi nếu khởi tạo thất bại
-//    }
-//}
-//
-//
-//
-//void Watchdog_Refresh(void) {
-//    HAL_IWDG_Refresh(&hiwdg); // Làm mới watchdog
-//}
-//
-//
-//uint8_t Is_Watchdog_Reset(void){
-//  if(counter_for_watchdog > 3){
-//	  return 1;
-//  }
-//  	  return 0;
-//}
-//
-//void Watchdog_Counting(void){
-//  counter_for_watchdog++;
-//}
-//
-//void Reset_Watchdog_Counting(void){
-//  counter_for_watchdog = 0;
-//}
-
