2f64e1ffe570b1ce38f4964645d502e6315bef76
phucann17 <139678917+phucann17@users.noreply.github.com>
2025-04-08 22:04:37 +0700

    Update button.h

--- MiniProject_VXL/Core/Inc/button.h
+++ MiniProject_VXL/Core/Inc/button.h
@@ -7,24 +7,21 @@
 
 #ifndef INC_BUTTON_H_
 #define INC_BUTTON_H_
-
 #include "main.h"
-
-
-
 #define NORMAL_STATE SET
 #define PRESSED_STATE RESET
 
 extern const int MAX_BUTTON;
-extern int button_flag[3];
+extern int button_flag[2];
+//extern int button_flagg;
 
 //DEFINE BUTTON GPIO A
 #define UP_BUTTON GPIO_PIN_8
 #define SET_BUTTON GPIO_PIN_9
 
 void getKeyInput();
-int isButtonPressed(int button_index);
-int isButtonLongPressed(int button_index);
+int isButtonPressed(int idx);
+int isButtonLongPressed(int idx);
 
 #endif /* INC_BUTTON_H_ */
 
