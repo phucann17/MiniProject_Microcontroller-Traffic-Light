6bf4063cdf331b22cd0ce925711a136aad606f5d
phucann17 <139678917+phucann17@users.noreply.github.com>
2025-04-09 21:53:00 +0700

    Update fsm_manual.h

--- MiniProject_VXL/Core/Inc/fsm_manual.h
+++ MiniProject_VXL/Core/Inc/fsm_manual.h
@@ -1,8 +1,6 @@
-#ifndef FSM_MANUAL_H_
-#define FSM_MANUAL_H_
-
+#ifndef INC_FSM_MANUAL_H_
+#define INC_FSM_MANUAL_H_
 #include "global.h"
-
 void run_manual();
 
-#endif
+#endif /* INC_FSM_MANUAL_H_ */
